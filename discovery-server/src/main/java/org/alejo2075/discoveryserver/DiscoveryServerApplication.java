59a93a79a8fabbf40fb612743c6e7131d2a95842
Alejo2075 <alejsant75@gmail.com>
2024-08-20 01:23:11 -0500

    Enable Eureka Server

--- discovery-server/src/main/java/org/alejo2075/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/org/alejo2075/discoveryserver/DiscoveryServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+Enable Eureka Server
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
