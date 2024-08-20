3c28b847334f6b734b7b636c58d18c4d956d99ca
Alejo2075 <alejsant75@gmail.com>
2024-08-20 01:29:49 -0500

    Enable Eureka Server

--- discovery-server/src/main/java/org/alejo2075/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/org/alejo2075/discoveryserver/DiscoveryServerApplication.java
@@ -2,9 +2,10 @@ package org.alejo2075.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-Enable Eureka Server
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
